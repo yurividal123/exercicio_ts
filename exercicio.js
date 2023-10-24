6231ed0fa606b89506a982c98b1b7e6b3574f590
Yuri Vidal <yurividal123@hotmail.com>
2023-10-24 17:54:02 -0300

    feito exercicio ts

---
+++ exercicio.js
@@ -0,0 +1,14 @@
+"use strict";
+function multiplicador(a, b) {
+    const resultado = a * b;
+    console.log(`o resultado de ${a} * ${b} é igual ${resultado}`);
+    return resultado;
+}
+multiplicador(2, 3);
+multiplicador(4, 6);
+function saudacao(nome) {
+    nome: nome;
+    console.log(`Olá ${nome}`);
+    return nome;
+}
+saudacao('yuri');
---
+++ exercicio.ts
@@ -0,0 +1,21 @@
+function multiplicador(a: number, b: number) {
+    const resultado: number = a * b;
+    console.log(`o resultado de ${a} * ${b} é igual ${resultado}`);
+
+    return resultado;
+}
+
+multiplicador(2,3);
+multiplicador(4,6);
+
+
+function saudacao(nome: string) {
+    nome: nome;
+    console.log(`Olá ${nome}`);
+
+    return nome;
+}
+
+saudacao('yuri');
+
+
